0f3b6ed723aa4c47a969ec5d8408e52978a34c06
Nathan Demick <ndemick@covermymeds.com>
2013-12-12 13:52:10 -0500

    Add basic test for Dashboard component

---
+++ spec/DashboardSpec.js
@@ -0,0 +1,25 @@
+/*jslint sloppy: true */
+/*global describe: false, it: false, expect: false, beforeEach: false, afterEach: false, $: false, runs: false, waitsFor: false */
+
+describe('PA Dashboard', function () {
+    beforeEach(function () {
+        $('body').append('<div id="dashboard"></div>');
+    });
+
+    it('displays a list of PAs', function () {
+
+        runs(function () {
+            $('#dashboard').dashboard({
+                ids: ['AY3MY7', 'PR6RY6', 'EY9BD9']
+            });
+        });
+
+        waitsFor(function () {
+            return $('#dashboard').children('.request').length > 0;
+        }, 'dashboard to return results', 2000);
+
+        runs(function () {
+            expect($('#dashboard').children('.request').length).not.toBe(0);
+        });
+    });
+});
