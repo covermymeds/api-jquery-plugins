594d93377a0293d44da4d850e62c989098ed8f30
Nathan Demick <ndemick@covermymeds.com>
2014-07-15 11:48:14 -0400

    New grunt CSS tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,41 @@ module.exports = function (grunt) {
         browserify: {
             dist: {
                 files: {
-                    'distribution/cover-my-meds-api-plugins.js': ['app/plugins.js']
+                    'distribution/js/cover-my-meds-api-plugins.js': ['app/plugins.js']
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'node_modules/select2-browserify/select2/select2-spinner.gif',
+                            'node_modules/select2-browserify/select2/select2.png'
+                        ],
+                        dest: 'distribution/css/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['node_modules/bootstrap/dist/fonts/**'],
+                        dest: 'distribution/fonts/',
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'distribution/css/stylesheet.css': [
+                        'app/stylesheets/main.css',
+                        'node_modules/bootstrap/dist/css/bootstrap.css',
+                        'node_modules/select2-browserify/select2/select2.css',
+                        'node_modules/select2-browserify/select2/select2-bootstrap.css'
+                    ]
                 }
             }
         },
@@ -17,19 +51,20 @@ module.exports = function (grunt) {
                 compress: true
             },
             dist: {
-                src: 'distribution/cover-my-meds-api-plugins.js',
-                dest: 'distribution/cover-my-meds-api-plugins.min.js'
+                src: 'distribution/js/cover-my-meds-api-plugins.js',
+                dest: 'distribution/js/cover-my-meds-api-plugins.min.js'
             }
         },
         jasmine: {
             customTemplate: {
-                src: ['distribution/cover-my-meds-api-plugins.js'],
+                src: ['distribution/js/cover-my-meds-api-plugins.js'],
                 options: {
-                    specs: 'spec/*Spec.js',
+                    specs: 'spec/*-spec.js',
                     vendor: [
-                        'node_modules/jquery/dist/jquery.min.js'
+                        'node_modules/jquery/dist/jquery.min.js',
+                        'spec/vendor/mock-ajax.js'
                     ],
-                    keepRunner: true
+                    keepRunner: false
                 }
             }
         },
@@ -39,22 +74,9 @@ module.exports = function (grunt) {
                     paths: ["app/stylesheets"]
                 },
                 files: {
-                    "distribution/stylesheet.css": "app/stylesheets/main.less"
+                    "app/stylesheets/main.css": "app/stylesheets/main.less"
                 }
             }
-            // production: {
-            //     options: {
-            //         paths: ["app/stylesheets"],
-            //         cleancss: true
-            //         // modifyVars: {
-            //         //     imgPath: '"http://mycdn.com/path/to/images"',
-            //         //     bgColor: 'red'
-            //         // }
-            //     },
-            //     files: {
-            //         "distribution/stylesheet.css": "app/stylesheets/main.less"
-            //     }
-            // }
         },
         watch: {
             scripts: {
@@ -67,14 +89,16 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-browserify');
-    // grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', 'watch');
-    grunt.registerTask('distribute', ['less', 'browserify', 'uglify']);
+    grunt.registerTask('css', ['copy', 'less', 'cssmin']);
+    grunt.registerTask('distribute', ['copy', 'less', 'cssmin', 'browserify', 'uglify']);
     grunt.registerTask('test', ['browserify', 'jasmine']);
 };
