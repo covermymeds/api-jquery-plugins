31693eb0f92f7a73e6fc4fd2eee92ad2f03ad8b6
Nathan Demick <ndemick@covermymeds.com>
2014-11-26 11:42:00 -0500

    Add XDomain for non-CORS browsers

--- app/plugins.js
+++ app/plugins.js
@@ -1,10 +1,20 @@
 /*jslint sloppy: true */
-/*global require: false */
+/*global require: false, $: false */
 
-window.$.fn.extend({
-    dashboard: require('./views/dashboard.js'),
-    showHelp: require('./views/help.js'),
-    createRequest: require('./views/create-request.js'),
-    drugSearch: require('./views/drug-search.js'),
-    formSearch: require('./views/form-search.js')
+if (!$.support.cors) {
+    var xdomain = require("./vendor/xdomain.min").xdomain;
+
+    xdomain.slaves({
+        'https://api.covermymeds.com': '/xhr-proxy.html'
+    });
+
+    $.support.cors = true;
+}
+
+$.fn.extend({
+    dashboard: require('./views/dashboard'),
+    showHelp: require('./views/help'),
+    createRequest: require('./views/create-request'),
+    drugSearch: require('./views/drug-search'),
+    formSearch: require('./views/form-search')
 });
